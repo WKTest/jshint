2bbd6177041e4934933a251a3996d56043b905b1
Wolfgang Kluge <wolfgang@klugesoftware.de>
2011-09-21 11:50:58 +0200

    Test whitespace in do-statement

--- tests/format.js
+++ tests/format.js
@@ -20,9 +20,11 @@ exports.format_whitespace = function () {
         
         .init(false, {checkformat: true})
         .run()
+            // function
             .hasError(2, "Expected whitespace '\xB7' between ')' and '{', but '\xB7\xB7' found.")
             .hasError(3, "Expected whitespace '\xB7' between 'return' and '+', but '\xB7\xB7\xB7' found.")
             .hasError(3, "Unexpected whitespace '\xB7\xB7' between '+' and '2'.")
+            // try
             .hasError(6, "Expected whitespace '\xB7' between 'try' and '{', but '\xB7\xB7' found.")
             .hasError(7, "Expected whitespace '\xB7' between '}' and 'catch', but '\xB7\xB7' found.")
             .hasError(7, "Expected whitespace '\xB7' between 'catch' and '(', but '\xB7\xB7\xB7' found.")
@@ -30,9 +32,14 @@ exports.format_whitespace = function () {
             .hasError(7, "Unexpected whitespace '\xB7\xB7\xB7\xB7\xB7' between 'ex' and ')'.")
             .hasError(7, "Expected whitespace '\xB7' between ')' and '{', but '\xB7\xB7\xB7\xB7\xB7...' found.")
             .hasError(11, "Expected whitespace '\xB7' between '}' and 'catch', but '' found.")
-        
+            // while
             .hasError(17, "Expected whitespace '\xB7' between 'while' and '(', but '\xB7\xB7' found.")
             .hasError(17, "Expected whitespace '\xB7' between ')' and '{', but '\xB7\xB7\xB7' found.")
-        .end()
+            // do
+            .hasError(20, "Expected whitespace '\xB7' between 'do' and '{', but '\xB7\xB7' found.")
+            .hasError(21, "Expected whitespace '\xB7' between '}' and 'while', but '\xB7\xB7' found.")
+            .hasError(21, "Expected whitespace '\xB7' between 'while' and '(', but '\xB7\xB7\xB7' found.")
+            .hasError(23, "Expected whitespace '\xB7' between '}' and 'while', but '' found.")
+         .end()
     ;
 };
