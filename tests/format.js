84e03680a2fe1a0932cf25c1d803d360b306609c
Wolfgang Kluge <wolfgang@klugesoftware.de>
2011-09-21 11:41:13 +0200

    Test whitespace in try/catch-block

--- tests/format.js
+++ tests/format.js
@@ -17,13 +17,19 @@ exports.format_whitespace = function () {
     
     helper(JSHINT, src)
         .init(true, {checkformat: false})
+        
         .init(false, {checkformat: true})
         .run()
             .hasError(2, "Expected whitespace '\xB7' between ')' and '{', but '\xB7\xB7' found.")
             .hasError(3, "Expected whitespace '\xB7' between 'return' and '+', but '\xB7\xB7\xB7' found.")
             .hasError(3, "Unexpected whitespace '\xB7\xB7' between '+' and '2'.")
             .hasError(6, "Expected whitespace '\xB7' between 'try' and '{', but '\xB7\xB7' found.")
-            .hasError(10, "Expected whitespace '\xB7' between ')' and '{', but '\xB7\xB7\xB7\xB7\xB7...' found.")
+            .hasError(7, "Expected whitespace '\xB7' between '}' and 'catch', but '\xB7\xB7' found.")
+            .hasError(7, "Expected whitespace '\xB7' between 'catch' and '(', but '\xB7\xB7\xB7' found.")
+            .hasError(7, "Unexpected whitespace '\xB7\xB7\xB7\xB7' between '(' and 'ex'.")
+            .hasError(7, "Unexpected whitespace '\xB7\xB7\xB7\xB7\xB7' between 'ex' and ')'.")
+            .hasError(7, "Expected whitespace '\xB7' between ')' and '{', but '\xB7\xB7\xB7\xB7\xB7...' found.")
+            .hasError(11, "Expected whitespace '\xB7' between '}' and 'catch', but '' found.")
         .end()
     ;
 };
